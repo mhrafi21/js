e3f3f066a2609d9761c3a8f3fd7f4a33d1d1128a
mhrafi21 <12mhrafi@gmail.com>
2025-02-11 20:59:21 +0600

    add: some function

---
+++ ts-generic/generic.ts
@@ -0,0 +1,13 @@
+type TPerson = {
+    name: string;
+    age:number;
+    profession: string;
+    hobby: string;
+}
+
+const person: TPerson = {
+    name: "Rafi",
+    age: 22,
+    profession: "student",
+    hobby: "Cricket"
+}
